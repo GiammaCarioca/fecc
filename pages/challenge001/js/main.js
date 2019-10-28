63700da65a07aa5a2470531e108d62750c853061
Thiago Giammattey <giammattey@gmail.com>
2019-10-28 14:18:34 -0300

    refactor markup and styles

--- pages/challenge001/js/main.js
+++ pages/challenge001/js/main.js
@@ -4,8 +4,8 @@
   const email = document.querySelector("#email");
   const btn = document.querySelector("#submit");
   const form = document.querySelector("form");
-  const alert = document.querySelector(".alert");
-  const check = document.querySelector(".check");
+  const alert = document.querySelector("#alert");
+  const check = document.querySelector("#check");
 
   email.addEventListener("input", function(event) {
     if (!email.validity.valid) {
