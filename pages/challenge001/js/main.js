ba132c20fce0f4f7f0109b90b11e6cd817ba29cd
Thiago Giammattey <giammattey@gmail.com>
2019-10-29 13:06:38 -0300

    more refactoring

--- pages/challenge001/js/main.js
+++ pages/challenge001/js/main.js
@@ -1,33 +1,33 @@
 (function() {
   "use strict";
 
-  const email = document.querySelector("#email");
-  const btn = document.querySelector("#submit");
   const form = document.querySelector("form");
+  const email = document.querySelector("input[type=email]");
   const alert = document.querySelector("#alert");
   const check = document.querySelector("#check");
 
-  email.addEventListener("input", function(event) {
-    if (!email.validity.valid) {
-      alert.style.opacity = 1;
-      alert.style.visibility = "visible";
-    }
-  });
+  email.addEventListener(
+    "input",
+    function() {
+      alert.style.visibility = "hidden";
+      alert.style.opacity = 0;
+    },
+    false
+  );
 
-  btn.addEventListener(
-    "click",
+  form.addEventListener(
+    "submit",
     function(event) {
+      event.preventDefault();
       if (!email.validity.valid) {
-        alert.style.opacity = 1;
         alert.style.visibility = "visible";
-        event.preventDefault();
-      } else {
-        form.style.display = "none";
-        check.style.opacity = 1;
-        check.style.visibility = "visible";
-        email.value = "";
-        event.preventDefault();
+        alert.style.opacity = 1;
+        return;
       }
+      form.style.display = "none";
+      check.style.visibility = "visible";
+      check.style.opacity = 1;
+      email.value = "";
     },
     false
   );
