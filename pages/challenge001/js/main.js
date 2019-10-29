35847349d231a8c3552dfcdb90f2930a90f9a078
Thiago Giammattey <giammattey@gmail.com>
2019-10-29 15:00:14 -0300

    fix focus

--- pages/challenge001/js/main.js
+++ pages/challenge001/js/main.js
@@ -1,7 +1,7 @@
 (function() {
   "use strict";
 
-  const form = document.querySelector("form");
+  const form = document.querySelector("#signup");
   const email = document.querySelector("input[type=email]");
   const alert = document.querySelector("#alert");
   const check = document.querySelector("#check");
@@ -22,9 +22,12 @@
       if (!email.validity.valid) {
         alert.style.visibility = "visible";
         alert.style.opacity = 1;
+        email.focus();
         return;
       }
-      form.style.display = "none";
+
+      form.style.position = "absolute";
+      form.style.opacity = 0;
       check.style.visibility = "visible";
       check.style.opacity = 1;
       email.value = "";
