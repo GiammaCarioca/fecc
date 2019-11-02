82d29ddcd6d88663fdd0516df96b77878020b96e
Thiago Giammattey <giammattey@gmail.com>
2019-11-02 11:18:30 -0300

    Add the novalidate attribute when the JS loads

--- pages/challenge001/js/main.js
+++ pages/challenge001/js/main.js
@@ -1,37 +1,40 @@
 (function() {
-  "use strict";
+	'use strict';
 
-  const form = document.querySelector("#signup");
-  const email = document.querySelector("input[type=email]");
-  const alert = document.querySelector("#alert");
-  const check = document.querySelector("#check");
+	const form = document.querySelector('#signup');
+	const email = document.querySelector('input[type=email]');
+	const alert = document.querySelector('#alert');
+	const check = document.querySelector('#check');
 
-  email.addEventListener(
-    "input",
-    function() {
-      alert.style.visibility = "hidden";
-      alert.style.opacity = 0;
-    },
-    false
-  );
+	if (!form) return;
+	form.setAttribute('novalidate', true);
 
-  form.addEventListener(
-    "submit",
-    function(event) {
-      event.preventDefault();
-      if (!email.validity.valid) {
-        alert.style.visibility = "visible";
-        alert.style.opacity = 1;
-        email.focus();
-        return;
-      }
-      check.focus();
-      check.style.outline = "none";
-      check.style.visibility = "visible";
-      check.style.opacity = 1;
-      email.value = "";
-      form.parentNode.removeChild(form);
-    },
-    false
-  );
+	email.addEventListener(
+		'input',
+		function() {
+			alert.style.visibility = 'hidden';
+			alert.style.opacity = 0;
+		},
+		false
+	);
+
+	form.addEventListener(
+		'submit',
+		function(event) {
+			event.preventDefault();
+			if (!email.validity.valid) {
+				alert.style.visibility = 'visible';
+				alert.style.opacity = 1;
+				email.focus();
+				return;
+			}
+			check.focus();
+			check.style.outline = 'none';
+			check.style.visibility = 'visible';
+			check.style.opacity = 1;
+			email.value = '';
+			form.parentNode.removeChild(form);
+		},
+		false
+	);
 })();
